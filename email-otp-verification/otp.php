129cd64266736d50f75028cc6ef221d930ad0e88
Tawrej Alam <118957522+tawrejalam786@users.noreply.github.com>
2023-11-09 18:23:45 +0530

    Add files via upload

---
+++ email-otp-verification/otp.php
@@ -0,0 +1,124 @@
+<!-- Code by Brave Coder - https://youtube.com/BraveCoder -->
+
+<?php
+    //Import PHPMailer classes into the global namespace
+    //These must be at the top of your script, not inside a function
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    session_start();
+    if (isset($_SESSION['SESSION_EMAIL'])) {
+        header("Location: welcome.php");
+        die();
+    }
+
+    //Load Composer's autoloader
+    require 'vendor/autoload.php';
+
+    include 'config.php';
+    $msg = "";
+
+    if(isset($_POST['otp'])){
+        $otp = mysqli_real_escape_string($conn, $_POST['OTP']);
+        if (mysqli_num_rows(mysqli_query($conn, "SELECT * FROM users WHERE code='{$otp}'")) > 0) {
+            $msg = "<div class='alert alert-danger'> OTP Verified Successfully.</div>";
+        } else {
+            $msg = "<div class='alert alert-danger'> Wrong OTP Entered Please Enter Right OTP.</div>";
+            // header("Location: register.php");
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <title>Login Form - Tech With Tawrej</title>
+    <!-- Meta tag Keywords -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    <meta name="keywords"
+        content="Login Form" />
+    <!-- //Meta tag Keywords -->
+
+    <link href="//fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+
+    <!--/Style-CSS -->
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+    <!--//Style-CSS -->
+
+    <script src="https://kit.fontawesome.com/af562a2a63.js" crossorigin="anonymous"></script>
+<style>
+    #Div2 {
+    display: none;
+  }
+</style>
+</head>
+
+<body>
+
+    <!-- form section start -->
+    <section class="w3l-mockup-form">
+        <div class="container">
+            <!-- /form -->
+            <div class="workinghny-form-grid">
+                <div class="main-mockup">
+                    <div class="alert-close">
+                        <span class="fa fa-close"></span>
+                    </div>
+                    <div class="w3l_form align-self">
+                        <div class="left_grid_info">
+                            <img src="images/image2.svg" alt="">
+                        </div>
+                    </div>
+                  
+
+                    <div class="content-wthree">
+                        <h2>Confirm OTP</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                        <?php echo $msg; ?>
+                        <form action="" method="post">
+                            <input type="text" class="name" name="OTP" placeholder="Enter Your OTP" required>
+
+                            <button name="otp" class="btn" type="submit">Register</button>
+                        </form>
+                        <div class="social-icons">
+                            <p>Have an account! <a href="index.php">Login</a>.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- //form -->
+        </div>
+    </section>
+    <!-- //form section start -->
+
+    <script src="js/jquery.min.js"></script>
+    <script>
+        $(document).ready(function (c) {
+            $('.alert-close').on('click', function (c) {
+                $('.main-mockup').fadeOut('slow', function (c) {
+                    $('.main-mockup').remove();
+                });
+            });
+        });
+
+        function switchVisible() {
+            if (document.getElementById('Div1')) {
+
+                if (document.getElementById('Div1').style.display == 'none') {
+                    document.getElementById('Div1').style.display = 'block';
+                    document.getElementById('Div2').style.display = 'none';
+                }
+                else {
+                    document.getElementById('Div1').style.display = 'none';
+                    document.getElementById('Div2').style.display = 'block';
+                }
+            }
+}
+    </script>
+
+</body>
+
+</html>
